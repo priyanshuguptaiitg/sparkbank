4caab135ff67c18b1a0d6adcb06dd7ae7d8ba362
PRIYANSHU GUPTA <74448032+priyanshuguptaiitg@users.noreply.github.com>
2021-08-17 20:34:25 +0530

    added more files

---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "database";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	if(!$conn)
+  die("DB NOT CONNECTED, Error : --> ".mysqli_connect_error());
